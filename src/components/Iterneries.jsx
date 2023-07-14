2c5441647b6e9b036e90d853cf169c1eb79fb34a
GangliaTechno <136331251+GangliaTechno@users.noreply.github.com>
2023-07-14 16:56:41 +0530

    14-07-2023 (Evening)

--- src/components/Iterneries.jsx
+++ src/components/Iterneries.jsx
@@ -3,30 +3,146 @@ import React from 'react'
 const Iterneries = () => {
     return (
         <>
-            <h6 className="section-title bg-white text-center px-3 fs-3 mt-5" style={{ color: "#37249D" }}>Quick Iternery Plan</h6>
+            <h6 className="bg-white text-center px-3 fs-3 mt-5" style={{ color: "#37249D" }}>Quick Iternery Plan</h6>
             <h2 className="mb-5 text-center">Here is your iternery plans</h2>
-            <div className='container justify-content-around mt-5'>
-                <div className='p-2 m-2 rounded' style={{ backgroundColor: "#dddee7" }}>
-                    <h4 className='section-title text-dark'>Hoode Beach</h4>
-                    <span class="badge badge-dark">10:30 - 11:30</span>
-                    <p>Spend some time in beach. Eat banana, enjoi life</p>
-                </div>
-                <div className='p-2 m-2 rounded' style={{ backgroundColor: "#dddee7" }}>
-                    <h4 className='section-title text-dark'>Malpe Fish Market</h4>
-                    <span class="badge badge-dark">11:30 - 12:30</span>
-                    <p>Buy some fish . Fry fish, Eat fish, enjoi life</p>
-                </div>
-                <div className='p-2 m-2 rounded' style={{ backgroundColor: "#dddee7" }}>
-                    <h4 className='section-title text-dark'>Hoode Beach</h4>
-                    <span class="badge badge-dark">10:30 - 11:30</span>
-                    <p>Spend some time in beach. Eat banana, enjoi life</p>
-                </div>
-                <div className='p-2 m-2 rounded' style={{ backgroundColor: "#dddee7" }}>
-                    <h4 className='section-title text-dark'>Malpe Fish Market</h4>
-                    <span class="badge badge-dark">11:30 - 12:30</span>
-                    <p>Buy some fish . Fry fish, Eat fish, enjoi life</p>
+            <div class="container py-5">
+                <div class="main-timeline-4 text-white">
+                    <div class="timeline-4 left-4">
+                        <div class="card">
+                            <div class="card-body p-4 text-white gradient-custom">
+                                <h4>Hoode Beach</h4>
+                                <span className="badge badge-dark mt-3">10:30 AM - 11:30 AM</span>
+                                <p className="mt-3">Spend some time in beach. Eat banana, enjoi life</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="timeline-4 right-4">
+                        <div class="card">
+                            <div class="card-body p-4 gradient-custom-4">
+                                <i class="fas fa-camera fa-2x mb-3"></i>
+                                <h4>8:00 AM</h4>
+                                <p class="small text-white-50 mb-4">May 18</p>
+                                <p>Lorem ipsum dolor sit amet, quo ei simul congue exerci, ad nec admodum perfecto
+                                    mnesarchum, vim ea mazim fierent detracto. Ea quis iuvaret expetendis his, te elit voluptua
+                                    dignissim
+                                    per, habeo iusto primis ea eam.
+                                </p>
+                                <h6 class="badge bg-light text-black mb-0">New</h6>
+                                <h6 class="badge bg-light text-black mb-0">Admin</h6>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="timeline-4 left-4">
+                        <div class="card gradient-custom">
+                            <div class="card-body p-4">
+                                <i class="fas fa-campground fa-2x mb-3"></i>
+                                <h4>7:25 PM</h4>
+                                <p class="small text-white-50 mb-4">May 6</p>
+                                <p>Lorem ipsum dolor sit amet, quo ei simul congue exerci, ad nec admodum perfecto
+                                    mnesarchum, vim ea mazim fierent detracto. Ea quis iuvaret expetendis his, te elit voluptua
+                                    dignissim
+                                    per, habeo iusto primis ea eam.
+                                </p>
+                                <h6 class="badge bg-light text-black mb-0">New</h6>
+                                <h6 class="badge bg-light text-black mb-0">Admin</h6>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="timeline-4 right-4">
+                        <div class="card gradient-custom-4">
+                            <div class="card-body p-4">
+                                <i class="fas fa-sun fa-2x mb-3"></i>
+                                <h4>3:55 PM</h4>
+                                <p class="small text-white-50 mb-4">Apr 26</p>
+                                <p>Lorem ipsum dolor sit amet, quo ei simul congue exerci, ad nec admodum perfecto
+                                    mnesarchum, vim ea mazim fierent detracto. Ea quis iuvaret expetendis his, te elit voluptua
+                                    dignissim
+                                    per, habeo iusto primis ea eam.
+                                </p>
+                                <h6 class="badge bg-light text-black mb-0">New</h6>
+                                <h6 class="badge bg-light text-black mb-0">Admin</h6>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="timeline-4 left-4">
+                        <div class="card gradient-custom">
+                            <div class="card-body p-4">
+                                <i class="fas fa-palette fa-2x mb-3"></i>
+                                <h4>5:24 PM</h4>
+                                <p class="small text-white-50 mb-4">Apr 12</p>
+                                <p>Lorem ipsum dolor sit amet, quo ei simul congue exerci, ad nec admodum perfecto
+                                    mnesarchum, vim ea mazim fierent detracto. Ea quis iuvaret expetendis his, te elit voluptua
+                                    dignissim
+                                    per, habeo iusto primis ea eam.
+                                </p>
+                                <h6 class="badge bg-light text-black mb-0">New</h6>
+                                <h6 class="badge bg-light text-black mb-0">Admin</h6>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="timeline-4 right-4">
+                        <div class="card gradient-custom-4">
+                            <div class="card-body p-4">
+                                <i class="fas fa-laugh-beam fa-2x mb-3"></i>
+                                <h4>11:25 AM</h4>
+                                <p class="small text-white-50 mb-4">Apr 11</p>
+                                <p>Lorem ipsum dolor sit amet, quo ei simul congue exerci, ad nec admodum perfecto
+                                    mnesarchum, vim ea mazim fierent detracto. Ea quis iuvaret expetendis his, te elit voluptua
+                                    dignissim
+                                    per, habeo iusto primis ea eam.
+                                </p>
+                                <h6 class="badge bg-light text-black mb-0">New</h6>
+                                <h6 class="badge bg-light text-black mb-0">Admin</h6>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="timeline-4 left-4">
+                        <div class="card gradient-custom">
+                            <div class="card-body p-4">
+                                <i class="fas fa-pizza-slice fa-2x mb-3"></i>
+                                <h4>12:30 PM</h4>
+                                <p class="small text-white-50 mb-4">Apr 5</p>
+                                <p>Lorem ipsum dolor sit amet, quo ei simul congue exerci, ad nec admodum perfecto
+                                    mnesarchum, vim ea mazim fierent detracto. Ea quis iuvaret expetendis his, te elit voluptua
+                                    dignissim
+                                    per, habeo iusto primis ea eam.
+                                </p>
+                                <h6 class="badge bg-light text-black mb-0">New</h6>
+                                <h6 class="badge bg-light text-black mb-0">Admin</h6>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
+
+
+
+            {/* <div className='container justify-content-around mt-5'>
+
+                <div className='p-4 mt-4 rounded' style={{ backgroundColor: "#dddee7" }}>
+                    <h4 className='text-dark'>Hoode Beach</h4>
+                    <span className="badge badge-dark mt-3">10:30 AM - 11:30 AM</span>
+                    <p className="mt-2">Spend some time in beach. Eat banana, enjoi life</p>
+                </div>
+
+                <div className='p-4 mt-4 rounded' style={{ backgroundColor: "#dddee7" }}>
+                    <h4 className='text-dark'>Malpe Fish Market</h4>
+                    <span className="badge badge-dark mt-3">11:30 AM - 12:30 PM</span>
+                    <p className="mt-2">Buy some fish . Fry fish, Eat fish, enjoi life</p>
+                </div>
+
+                <div className='p-4 mt-4 rounded' style={{ backgroundColor: "#dddee7" }}>
+                    <h4 className='text-dark'>Hoode Beach</h4>
+                    <span className="badge badge-dark mt-3">12:30 PM - 01:30 PM</span>
+                    <p className="mt-2">Spend some time in beach. Eat banana, enjoi life</p>
+                </div>
+
+                <div className='p-4 mt-4 rounded' style={{ backgroundColor: "#dddee7" }}>
+                    <h4 className='text-dark'>Malpe Fish Market</h4>
+                    <span className="badge badge-dark mt-3">01:30 PM - 02:30 PM</span>
+                    <p className="mt-2">Buy some fish . Fry fish, Eat fish, enjoi life</p>
+                </div>
+            </div> */}
         </>
     )
 }
