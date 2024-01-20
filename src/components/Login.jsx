587e60f28a634e6f3971fc295d305244b48edd9c
Abhinav Raj <abhinavraj1208@gmail.com>
2024-01-20 11:57:40 +0530

    Added a Sign in with email and password button Added a close login popup button

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -19,6 +19,18 @@ const Login = () => {
         theme: "dark",
     });
 
+    //When Signin with Email and Password is clicked
+    const mailnotify = () => toast.info('Feature Not supported yet.', {
+        position: "top-center",
+        autoClose: 3000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "dark",
+    });
+
     //When Login Clicked Username to be displayed 
     const {
         isLoggedIn,
@@ -89,9 +101,9 @@ const Login = () => {
                 <div className="modal-dialog modal-dialog-centered" role="document">
                     <div className="modal-content">
                         <div className="modal-header">
-                            <h4 className="modal-title w-100 text-center" id="exampleModalLongTitle">Login/SignIn</h4>
-                            <button type="button" ref={closebtnRef} className="close" data-dismiss="modal" aria-label="Close" hidden>
-                                <span aria-hidden="true">&times;</span>
+                            <h4 className="modal-title w-100 text-center" id="exampleModalLongTitle">Login/SignUp</h4>
+                            <button type="button" ref={closebtnRef} className="close" data-dismiss="modal" aria-label="Close"><i class="fa-solid fa-circle-xmark"></i>
+                                {/* <span aria-hidden="true">&times;</span> */}
                             </button>
                         </div>
                         <div className="modal-body signin_modal">
@@ -100,22 +112,26 @@ const Login = () => {
                             </div>
                             <div className="row">
                                 <div className="col-md-12 mt-3">
-                                    <a onClick={handleAnonymousSignIn} className="btn-lg btn-guest rounded-pill btn-block text-uppercase fs-6" href="#" id="btnGuest"><img src={guestImage} className="mb-1 me-2" />Continue as Guest</a>
+                                    <a onClick={handleAnonymousSignIn} className="btn-lg btn-guest rounded-pill btn-block fs-6" href="#" id="btnGuest"><img src={guestImage} className="mb-1 me-2" />Continue as Guest</a>
                                 </div>
                                 <p className="loginOR mt-3">OR</p>
-                                <p className='text-center'>For Better Experience, Login/SignIn</p>
+                                <p className='text-center'>For Better Experience, Login/SignUp</p>
                                 <div className="col-md-12">
-                                    <a onClick={handleGoogleSignIn} className="btn-lg btn-google rounded-pill btn-block text-uppercase fs-6" href="#">
+                                    <a onClick={handleGoogleSignIn} className="btn-lg btn-google rounded-pill btn-block fs-6" href="#">
                                         <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="27" height="27" viewBox="0 0 48 48" className="mb-1 me-2">
                                             <path fill="#FFC107" d="M43.611,20.083H42V20H24v8h11.303c-1.649,4.657-6.08,8-11.303,8c-6.627,0-12-5.373-12-12c0-6.627,5.373-12,12-12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657C34.046,6.053,29.268,4,24,4C12.955,4,4,12.955,4,24c0,11.045,8.955,20,20,20c11.045,0,20-8.955,20-20C44,22.659,43.862,21.35,43.611,20.083z"></path><path fill="#FF3D00" d="M6.306,14.691l6.571,4.819C14.655,15.108,18.961,12,24,12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657C34.046,6.053,29.268,4,24,4C16.318,4,9.656,8.337,6.306,14.691z"></path><path fill="#4CAF50" d="M24,44c5.166,0,9.86-1.977,13.409-5.192l-6.19-5.238C29.211,35.091,26.715,36,24,36c-5.202,0-9.619-3.317-11.283-7.946l-6.522,5.025C9.505,39.556,16.227,44,24,44z"></path><path fill="#1976D2" d="M43.611,20.083H42V20H24v8h11.303c-0.792,2.237-2.231,4.166-4.087,5.571c0.001-0.001,0.002-0.001,0.003-0.002l6.19,5.238C36.971,39.205,44,34,44,24C44,22.659,43.862,21.35,43.611,20.083z"></path>
                                         </svg>Sign in using Google
                                     </a>
                                 </div>
                                 <div className="col-md-12 mt-3">
-                                    <a onClick={applenotify} className="btn-lg btn-apple rounded-pill btn-block text-uppercase fs-6 my-3" href="#">
+                                    <a onClick={applenotify} className="btn-lg btn-apple rounded-pill btn-block fs-6 my-3" href="#">
                                         <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="27" height="27" viewBox="0 0 50 50" className="mb-1 me-3">
                                             <path d="M 44.527344 34.75 C 43.449219 37.144531 42.929688 38.214844 41.542969 40.328125 C 39.601563 43.28125 36.863281 46.96875 33.480469 46.992188 C 30.46875 47.019531 29.691406 45.027344 25.601563 45.0625 C 21.515625 45.082031 20.664063 47.03125 17.648438 47 C 14.261719 46.96875 11.671875 43.648438 9.730469 40.699219 C 4.300781 32.429688 3.726563 22.734375 7.082031 17.578125 C 9.457031 13.921875 13.210938 11.773438 16.738281 11.773438 C 20.332031 11.773438 22.589844 13.746094 25.558594 13.746094 C 28.441406 13.746094 30.195313 11.769531 34.351563 11.769531 C 37.492188 11.769531 40.8125 13.480469 43.1875 16.433594 C 35.421875 20.691406 36.683594 31.78125 44.527344 34.75 Z M 31.195313 8.46875 C 32.707031 6.527344 33.855469 3.789063 33.4375 1 C 30.972656 1.167969 28.089844 2.742188 26.40625 4.78125 C 24.878906 6.640625 23.613281 9.398438 24.105469 12.066406 C 26.796875 12.152344 29.582031 10.546875 31.195313 8.46875 Z"></path>
                                         </svg>Sign in using Apple
+                                    </a>
+                                </div>
+                                <div className="col-md-12 mt-3">
+                                    <a onClick={applenotify} className="btn-lg btn-mail rounded-pill btn-block fs-6 my-3" href="#"><i class="fa-regular fa-envelope fa-lg"></i> Sign in using Email and Password
                                     </a>
                                 </div>
                             </div>
