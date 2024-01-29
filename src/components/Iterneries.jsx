f0cff814f40581130e32ee977b4bad26233c1e33
tamil-eniyan <eniyan2001@gmail.com>
2024-01-29 10:08:54 +0530

    Sending three requests to the  backend

--- src/components/Iterneries.jsx
+++ src/components/Iterneries.jsx
@@ -53,6 +53,27 @@ const Iterneries = (props) => {
                         )): <h2 style={{color:"black",textAlign:"center"}}>Generating plan...</h2>
                     }
                 </div>
+
+
+      <div className="main-timeline-4 text-white" style={{content:null}}>
+                    {
+                        response2 !== null ?
+                        response2.plan.map((place, index) => (
+                            <div className={`timeline-4 ${index % 2 === 0 ? 'left-4' : 'right-4'}`} key={index}>
+                                <div className="card">
+                                    <div className="card-body p-4 gradient-custom">
+                                        <h4>Place: <span>{place === null ? "NULL" : capitalizeFirstLetter(place.location)}</span></h4>
+                                        Timings: <span className="badge badge-dark mt-3 p-2">{place === null ? "NULL" : place.time}</span>
+                                        <p className="mt-3">Activity: <span>{place === null ? "NULL" : capitalizeFirstLetter(place.activity)}</span></p>
+                                        <p>Distance: <span>{place === null ? "NULL" : place.distance}</span></p>
+                                        <p>Travel-Time: <span>{place === null ? "NULL" : place["travel time"]}</span></p>
+                                        <a className="btn" style={{ backgroundColor: "white", color: "black" }} href="#" role="button">Open in Maps</a>
+                                    </div>
+                                </div>
+                            </div>
+                        )): <h2 style={{color:"black",textAlign:"center"}}>Generating plan...</h2>
+                    }
+                </div>
             </div>
         </>
     )
@@ -64,3 +85,4 @@ const Iterneries = (props) => {
 
 
 export default Iterneries
+
